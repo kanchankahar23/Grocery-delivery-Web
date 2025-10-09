82270ef47c8d18d798ac4214a15f73a21dd4d900
kanchan kahar <kaharkanchan884@gmail.com>
2025-10-09 15:25:52 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
           />
         </Routes>
       </div>
-      <Home />
+  
     </div>
   )
 }
---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BestSeller = () => {
+    return (
+        <div>
+            <p>Best Sellers</p>
+            <div>
+                <div>
+                    <img src='' alt=''></img>
+                    <div>
+                    <p>Vegetables</p>
+                    <h1>Potato 500g</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BestSeller
